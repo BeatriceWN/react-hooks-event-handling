ad56e8c908f45f4f9ed8643f16d7a8e8f5c35b0a
Beatrice Wambui <bettyneema2@gmail.com>
2025-08-03 08:01:11 +0300

    initial commit adding event listeners

--- src/components/Tickler.js
+++ src/components/Tickler.js
@@ -1,11 +1,20 @@
 import React from "react";
 
-function Tickler() {
-  function tickle() {
-    console.log("Teehee!");
-  }
+//good if you have more than one line of code to run in your event handler,
+// function Tickler() {
+//   function tickle() {
+//     console.log("Teehee!");
+//   }
 
-  return <button>Tickle me!</button>;
+//   return <button onClick={tickle}>Tickle me!</button>;
+// }
+
+//-------------------------------------//
+
+//use an arrow function to define an event handler inline, and pass it to the event listener:
+// good choice if your event handler doesn't need to handle much logic
+function Tickler() {
+  return <button onClick={() => console.log("Teehee!")}>Tickle me!</button>;
 }
 
 export default Tickler;
